e1f7bdba467940d7405b7b6cf58fac01d043fb06
Sandeep Kumar <san07364@gmail.com>
2023-01-11 12:15:07 +0530

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,83 +29,83 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _openAiClient = OpenAIClient(OpenAIConfig(
-      apiKey: "---your-api-key-here---",
-      organizationId: "---your-organization-id-here---"));
+  final _openAiClient = OpenAIClient(
+    OpenAIConfig(
+        apiKey: "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",
+        organizationId: "org-PaB4ld1VpvP0yEYsRB5ZhBOZ"),
+  );
 
   final _textEditingController = TextEditingController();
-  final _scrollController = ScrollController();
+
   final _completionRequest =
       CreateCompletionRequest(model: "text-davinci-003", maxTokens: 2048);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("ChatGPT")),
+      appBar: AppBar(
+        title: const Text("ChatGPT"),
+      ),
       body: ChatGPTBuilder(
-        completionRequest: _completionRequest,
-        openAIClient: _openAiClient,
-        builder: (context, messages, onSend) {
-          return Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: ListView.separated(
-                    itemCount: messages.length,
-                    controller: _scrollController,
-                    separatorBuilder: (context, index) => const SizedBox(
-                          height: 5,
+          completionRequest: _completionRequest,
+          openAIClient: _openAiClient,
+          builder: (context, messages, onSend) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: ListView.separated(
+                      itemCount: messages.length,
+                      separatorBuilder: (context, index) => const SizedBox(
+                            height: 5,
+                          ),
+                      itemBuilder: (context, index) {
+                        var isSender = !messages[index].fromChatGPT;
+                        return BubbleSpecialThree(
+                          isSender: isSender,
+                          text: messages[index].message,
+                          color:
+                              isSender ? const Color(0xFF1B97F3) : Colors.white,
+                          tail: true,
+                          textStyle: TextStyle(
+                              color: isSender ? Colors.white : Colors.grey[800],
+                              fontSize: 16),
+                        );
+                      }),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                              hintText: "Message",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20))),
                         ),
-                    itemBuilder: (context, index) {
-                      var isSender = !messages[index].fromChatGPT;
-                      return BubbleSpecialThree(
-                        isSender: isSender,
-                        text: messages[index].message,
-                        color:
-                            isSender ? const Color(0xFF1B97F3) : Colors.white,
-                        tail: true,
-                        textStyle: TextStyle(
-                            color: isSender ? Colors.white : Colors.grey[800],
-                            fontSize: 16),
-                      );
-                    }),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        controller: _textEditingController,
-                        decoration: InputDecoration(
-                            hintText: "Message",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20))),
                       ),
                     ),
-                  ),
-                  FloatingActionButton(
+                    FloatingActionButton(
                       onPressed: () {
                         if (_textEditingController.text.trim().isEmpty) return;
-                        onSend(_textEditingController.text).whenComplete(() {
-                          _scrollController.jumpTo(
-                              _scrollController.position.maxScrollExtent);
-                        });
+                        onSend(_textEditingController.text);
                         FocusScope.of(context).unfocus();
                         _textEditingController.clear();
                       },
-                      child: const Icon(Icons.send)),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                ],
-              ),
-            ],
-          );
-        },
-      ),
+                      child: const Icon(Icons.send),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                  ],
+                ),
+              ],
+            );
+          }),
     );
   }
 }
