651ee5243eec3810c247d5d8072a756b543c7af8
Justin-Sunny01 <117711435+Justin-Sunny01@users.noreply.github.com>
2023-01-12 18:53:25 +0530

    exports added

--- lib/sitesurface_flutter_openai.dart
+++ lib/sitesurface_flutter_openai.dart
@@ -4,5 +4,8 @@ export 'src/openai_client.dart';
 export 'src/completion/models/create_completion_request.dart';
 export 'src/edits/models/create_edit_request.dart';
 export 'src/images/models/create_image_request.dart';
+export 'src/images/models/create_image_edit_request.dart';
+export 'src/images/models/create_image_variation_request.dart';
+export 'src/fine_tunes/models/create_fine_tune_request.dart';
 export "src/widgets/chatgpt_builder.dart";
 export 'src/common/models/openai_config.dart';
